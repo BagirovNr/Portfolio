bc2e3891160e8f339a9b818f4b7d0e140a7bde3c
Nadir <nadir.bagirov2000@gmail.com>
2024-07-17 16:50:35 +0400

    fix : :fire: responsive design

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
     return (
-        <div>About</div>
+        <div className='pt-20'>About</div>
     )
 }
 
--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div className=''>
+        <div className='pt-20'>
             <h1 className='text-white text-xl'>Contact</h1></div>
     )
 }
