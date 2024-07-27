63b98093bf1acb7b5d9776bbb06709db80f70e47
Nadir <nadir.bagirov2000@gmail.com>
2024-07-27 15:53:35 +0400

    fix : :fire:fix bug

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -127,7 +127,7 @@ const Form: React.FC = () => {
           {errors[field.name] && (
             <div className='flex gap-1 items-center'>
               <Info size={16} weight='fill' color='#F97066' />
-              <p className='text-red-500 text-base leading-4 font-medium mt-1'>{errors[field.name]?.message}</p>
+              <p className="text-[#F97066] text-sm leading-4 font-medium">{(errors[field.name] as any)?.message}</p>
             </div>
           )}
         </div>
